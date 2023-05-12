134437b817807e490807914dedeb9ba5798ea1e1
Francis Aron Santa Cruz De la Cruz <aronsantacruz95@gmail.com>
2023-05-12 17:17:41 -0500

    Update scrap_SSI_F12B_PMI_PVD v2.py

--- scrap_SSI_F12B_PMI_PVD v2.py
+++ scrap_SSI_F12B_PMI_PVD v2.py
@@ -2,10 +2,9 @@ import time
 import pandas as pd
 import numpy as np
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 from datetime import date
-    
+
 # para contabilizar tiempo de demora
 start = time.time() # inicia toma de tiempo
 
@@ -24,7 +23,6 @@ PATH_INPUT = 'C:/Users/a/Documents/aron/Data/'
 PATH_OUTPUT = 'C:/Users/a/Documents/aron/Reportes/'
 # nombre del archivo output
 FILE_OUTPUT1 = 'infoF12BSSIPMI_{}{}.xlsx'.format(d1,sufijo)
-FILE_OUTPUT2 = 'infoF12BSSIPMICAT_{}{}.xlsx'.format(d1,sufijo)
 # nombre del archivo con CUIs
 FILE_CUI = 'cuis_2023{}.xlsx'.format(sufijo)
 # tiempo que deja cargar cada página
@@ -34,8 +32,6 @@ timesleep=1.5
 
 driver = webdriver.Chrome()
 BBDD = pd.DataFrame()
-
-#Ncui = "2154492"
 
 ## INICIA BUCLE
 
@@ -379,7 +375,6 @@ for Ncui in cuis:
     del dfPMI
     
     BBDD = pd.concat([BBDD, infoF12BSSI], axis=0, sort=False)
-    #BBDD = BBDD.append(infoF12BSSI)
     del infoF12BSSI
     
     # COSTOS
@@ -401,103 +396,6 @@ BBDD['cfianza'] = pd.to_numeric(BBDD['cfianza'])
 BBDD['montototal'] = pd.to_numeric(BBDD['montototal'])
 BBDD['PIM_ACT'] = pd.to_numeric(BBDD['PIM_ACT'])
 
-BBDD.loc[BBDD['PROG_ACT_ENE']=='-','PROG_ACT_ENE'] = '0'
-# BBDD['PROG_ACT_ENE'] = BBDD['PROG_ACT_ENE'].replace('-','0')
-BBDD['PROG_ACT_ENE'] = pd.to_numeric(BBDD['PROG_ACT_ENE'])
-
-BBDD.loc[BBDD['DEV_ENE']=='-','DEV_ENE'] = '0'
-# BBDD['DEV_ENE'] = BBDD['DEV_ENE'].replace('-','0')
-BBDD['DEV_ENE'] = pd.to_numeric(BBDD['DEV_ENE'])
-
-BBDD.loc[BBDD['PROG_ACT_FEB']=='-','PROG_ACT_FEB'] = '0'
-# BBDD['PROG_ACT_FEB'] = BBDD['PROG_ACT_FEB'].replace('-','0')
-BBDD['PROG_ACT_FEB'] = pd.to_numeric(BBDD['PROG_ACT_FEB'])
-
-BBDD.loc[BBDD['DEV_FEB']=='-','DEV_FEB'] = '0'
-# BBDD['DEV_FEB'] = BBDD['DEV_FEB'].replace('-','0')
-BBDD['DEV_FEB'] = pd.to_numeric(BBDD['DEV_FEB'])
-
-BBDD.loc[BBDD['PROG_ACT_MAR']=='-','PROG_ACT_MAR'] = '0'
-# BBDD['PROG_ACT_MAR'] = BBDD['PROG_ACT_MAR'].replace('-','0')
-BBDD['PROG_ACT_MAR'] = pd.to_numeric(BBDD['PROG_ACT_MAR'])
-
-BBDD.loc[BBDD['DEV_MAR']=='-','DEV_MAR'] = '0'
-# BBDD['DEV_MAR'] = BBDD['DEV_MAR'].replace('-','0')
-BBDD['DEV_MAR'] = pd.to_numeric(BBDD['DEV_MAR'])
-
-BBDD.loc[BBDD['PROG_ACT_ABR']=='-','PROG_ACT_ABR'] = '0'
-# BBDD['PROG_ACT_ABR'] = BBDD['PROG_ACT_ABR'].replace('-','0')
-BBDD['PROG_ACT_ABR'] = pd.to_numeric(BBDD['PROG_ACT_ABR'])
-
-BBDD['DEV_ABR'] = BBDD['DEV_ABR'].replace("--67,845","-67845")
-BBDD.loc[BBDD['DEV_ABR']=='-','DEV_ABR'] = '0'
-# BBDD['DEV_ABR'] = BBDD['DEV_ABR'].replace('-','0')
-BBDD['DEV_ABR'] = pd.to_numeric(BBDD['DEV_ABR'])
-
-BBDD.loc[BBDD['PROG_ACT_MAY']=='-','PROG_ACT_MAY'] = '0'
-# BBDD['PROG_ACT_MAY'] = BBDD['PROG_ACT_MAY'].replace('-','0')
-BBDD['PROG_ACT_MAY'] = pd.to_numeric(BBDD['PROG_ACT_MAY'])
-
-BBDD.loc[BBDD['DEV_MAY']=='-','DEV_MAY'] = '0'
-# BBDD['DEV_MAY'] = BBDD['DEV_MAY'].replace('-','0')
-BBDD['DEV_MAY'] = pd.to_numeric(BBDD['DEV_MAY'])
-
-BBDD.loc[BBDD['PROG_ACT_JUN']=='-','PROG_ACT_JUN'] = '0'
-# BBDD['PROG_ACT_JUN'] = BBDD['PROG_ACT_JUN'].replace('-','0')
-BBDD['PROG_ACT_JUN'] = pd.to_numeric(BBDD['PROG_ACT_JUN'])
-
-BBDD.loc[BBDD['DEV_JUN']=='-','DEV_JUN'] = '0'
-# BBDD['DEV_JUN'] = BBDD['DEV_JUN'].replace('-','0')
-BBDD['DEV_JUN'] = pd.to_numeric(BBDD['DEV_JUN'])
-
-BBDD.loc[BBDD['PROG_ACT_JUL']=='-','PROG_ACT_JUL'] = '0'
-# BBDD['PROG_ACT_JUL'] = BBDD['PROG_ACT_JUL'].replace('-','0')
-BBDD['PROG_ACT_JUL'] = pd.to_numeric(BBDD['PROG_ACT_JUL'])
-
-BBDD.loc[BBDD['DEV_JUL']=='-','DEV_JUL'] = '0'
-# BBDD['DEV_JUL'] = BBDD['DEV_JUL'].replace('-','0')
-BBDD['DEV_JUL'] = pd.to_numeric(BBDD['DEV_JUL'])
-
-BBDD.loc[BBDD['PROG_ACT_AGO']=='-','PROG_ACT_AGO'] = '0'
-# BBDD['PROG_ACT_AGO'] = BBDD['PROG_ACT_AGO'].replace('-','0')
-BBDD['PROG_ACT_AGO'] = pd.to_numeric(BBDD['PROG_ACT_AGO'])
-
-BBDD.loc[BBDD['DEV_AGO']=='-','DEV_AGO'] = '0'
-# BBDD['DEV_AGO'] = BBDD['DEV_AGO'].replace('-','0')
-BBDD['DEV_AGO'] = pd.to_numeric(BBDD['DEV_AGO'])
-
-BBDD.loc[BBDD['PROG_ACT_SET']=='-','PROG_ACT_SET'] = '0'
-# BBDD['PROG_ACT_SET'] = BBDD['PROG_ACT_SET'].replace('-','0')
-BBDD['PROG_ACT_SET'] = pd.to_numeric(BBDD['PROG_ACT_SET'])
-
-BBDD.loc[BBDD['DEV_SET']=='-','DEV_SET'] = '0'
-# BBDD['DEV_SET'] = BBDD['DEV_SET'].replace('-','0')
-BBDD['DEV_SET'] = pd.to_numeric(BBDD['DEV_SET'])
-
-BBDD.loc[BBDD['PROG_ACT_OCT']=='-','PROG_ACT_OCT'] = '0'
-# BBDD['PROG_ACT_OCT'] = BBDD['PROG_ACT_OCT'].replace('-','0')
-BBDD['PROG_ACT_OCT'] = pd.to_numeric(BBDD['PROG_ACT_OCT'])
-
-BBDD.loc[BBDD['DEV_OCT']=='-','DEV_OCT'] = '0'
-# BBDD['DEV_OCT'] = BBDD['DEV_OCT'].replace('-','0')
-BBDD['DEV_OCT'] = pd.to_numeric(BBDD['DEV_OCT'])
-
-BBDD.loc[BBDD['PROG_ACT_NOV']=='-','PROG_ACT_NOV'] = '0'
-# BBDD['PROG_ACT_NOV'] = BBDD['PROG_ACT_NOV'].replace('-','0')
-BBDD['PROG_ACT_NOV'] = pd.to_numeric(BBDD['PROG_ACT_NOV'])
-
-BBDD.loc[BBDD['DEV_NOV']=='-','DEV_NOV'] = '0'
-# BBDD['DEV_NOV'] = BBDD['DEV_NOV'].replace('-','0')
-BBDD['DEV_NOV'] = pd.to_numeric(BBDD['DEV_NOV'])
-
-BBDD.loc[BBDD['PROG_ACT_DIC']=='-','PROG_ACT_DIC'] = '0'
-# BBDD['PROG_ACT_DIC'] = BBDD['PROG_ACT_DIC'].replace('-','0')
-BBDD['PROG_ACT_DIC'] = pd.to_numeric(BBDD['PROG_ACT_DIC'])
-
-BBDD.loc[BBDD['DEV_DIC']=='-','DEV_DIC'] = '0'
-# BBDD['DEV_DIC'] = BBDD['DEV_DIC'].replace('-','0')
-BBDD['DEV_DIC'] = pd.to_numeric(BBDD['DEV_DIC'])
-
 BBDD['PROG_ACT_TOT'] = pd.to_numeric(BBDD['PROG_ACT_TOT'])
 BBDD['DEV_TOT'] = pd.to_numeric(BBDD['DEV_TOT'])
 BBDD['SALDO'] = pd.to_numeric(BBDD['SALDO'])
@@ -505,106 +403,18 @@ BBDD['DEV_ACUM'] = pd.to_numeric(BBDD['DEV_ACUM'])
 
 BBDD['AV_FIN_ACUM'] = BBDD['DEV_ACUM']/BBDD['montototal']
 
+# Corrige los valores extraños (espacios de más u otros):
+BBDD['fdaei'] = BBDD['fdaei'].apply(lambda x: '' if 'Fec' in str(x) else x)
+BBDD['et'] = BBDD['et'].apply(lambda x: 'NO' if 'NO' in str(x) else x)
+BBDD['et'] = BBDD['et'].apply(lambda x: 'SI' if 'SI' in str(x) else x)
+BBDD['registroseg'] = BBDD['registroseg'].apply(lambda x: 'SI' if 'SI' in str(x) else x)
+BBDD['registroseg'] = BBDD['registroseg'].apply(lambda x: 'NO' if 'NO' in str(x) else x)
+BBDD['PMI'] = BBDD['PMI'].apply(lambda x: 'NO' if 'NO' in str(x) else x)
+BBDD['PMI'] = BBDD['PMI'].apply(lambda x: 'SI' if 'SI' in str(x) else x)
+
 BBDD = BBDD[['cui','codsnip','nominv','opmi','uf','uei','fecharegistro','feciniejec','fecfinejec','fum','fdaei','fechaviab','montoviable','et','tipoinv','modalidadejec','estadoinv','situacion','decretoemerg','registrocierre','cadfun','beneficiarios','registroseg','PMI','pmi01','pmi02','pmi03','cia','concurr','laudo','cfianza','montototal','PIM_ACT','PROG_ACT_ENE','DEV_ENE','PROG_ACT_FEB','DEV_FEB','PROG_ACT_MAR','DEV_MAR','PROG_ACT_ABR','DEV_ABR','PROG_ACT_MAY','DEV_MAY','PROG_ACT_JUN','DEV_JUN','PROG_ACT_JUL','DEV_JUL','PROG_ACT_AGO','DEV_AGO','PROG_ACT_SET','DEV_SET','PROG_ACT_OCT','DEV_OCT','PROG_ACT_NOV','DEV_NOV','PROG_ACT_DIC','DEV_DIC','PROG_ACT_TOT','DEV_TOT','SALDO','DEV_ACUM','AV_FIN_ACUM','PRIMER_DEV','ULT_DEV','prioridad','prelacion','costoactpmi','devacum2022pmi', 'pim2023', 'pmi2023', 'pmi2024', 'pmi2025', 'pmi2026']]
 
 BBDD.to_excel('{}{}'.format(PATH_OUTPUT,FILE_OUTPUT1),sheet_name='BD',index=False)
-
-
-
-
-
-# BBDDca = pd.DataFrame()
-# web = "https://apps5.mineco.gob.pe/bingos/seguimiento_pi/Navegador/default.aspx?y=2023&ap=ActProy"
-# driver.get(web)
-# driver.switch_to.frame('frame0')
-# driver.find_element("id", "ctl00_CPH1_BtnProdProy").click()
-# time.sleep(timesleep)
-# for Ncui in cuis:
-    
-#     print(Ncui)
-#     print('1')
-#     driver.find_element("id", "ctl00_CPH1_TxtSearch").send_keys(Ncui)
-#     print('2')
-#     time.sleep(timesleep)
-#     driver.find_element("id", "ctl00_CPH1_BtnSearchByCode").click()
-#     time.sleep(timesleep)
-#     #driver.find_element("name", "grp1").click()
-    
-#     _infopptotot = np.array([[0]])
-#     __infopptotot = pd.DataFrame(_infopptotot)
-#     del _infopptotot
-    
-#     try:
-#         pptotot = driver.find_element("name", "grp1").get_attribute("value")
-#     except:
-#         pptotot = '{}/ / / / / / / / / '.format(Ncui)
-#         driver.find_element("id", "ctl00_CPH1_Image1").click()
-#         time.sleep(timesleep)
-#         driver.switch_to.frame('frame0')
-#         driver.find_element("id", "ctl00_CPH1_BtnProdProy").click()
-#         pass
-    
-#     __infopptotot['pptotot'] = pptotot
-#     del pptotot
-#     tabpptotot = __infopptotot['pptotot'].str.split("/", expand = True)
-#     del __infopptotot
-#     BBDDca = pd.concat([BBDDca, tabpptotot], axis=0, sort=False)
-    
-#     time.sleep(timesleep)
-#     print('3')
-#     try:
-#         driver.find_element("id", "ctl00_CPH1_TxtSearch").click()
-#         driver.find_element("id", "ctl00_CPH1_TxtSearch").send_keys(Keys.END)
-#         driver.find_element("id", "ctl00_CPH1_TxtSearch").send_keys(Keys.BACKSPACE)
-#         driver.find_element("id", "ctl00_CPH1_TxtSearch").send_keys(Keys.BACKSPACE)
-#         driver.find_element("id", "ctl00_CPH1_TxtSearch").send_keys(Keys.BACKSPACE)
-#         driver.find_element("id", "ctl00_CPH1_TxtSearch").send_keys(Keys.BACKSPACE)
-#         driver.find_element("id", "ctl00_CPH1_TxtSearch").send_keys(Keys.BACKSPACE)
-#         driver.find_element("id", "ctl00_CPH1_TxtSearch").send_keys(Keys.BACKSPACE)
-#         driver.find_element("id", "ctl00_CPH1_TxtSearch").send_keys(Keys.BACKSPACE)
-#     except:
-#         driver.find_element("id", "ctl00_CPH1_Image1").click()
-#         time.sleep(timesleep)
-#         driver.switch_to.frame('frame0')
-#         driver.find_element("id", "ctl00_CPH1_BtnProdProy").click()
-#         pass
-#     print('4')
-
-
-# BBDDca.columns = ['cui','ca_costo','ca_devacum21','ca_dev22','ca_pia23','ca_pim23','ca_dev23','ca_avance23','ca_devacumtot','ca_avancetot']
-# BBDDca = BBDDca.drop(['ca_avance23','ca_avancetot'], axis=1)
-
-# BBDDca['ca_costo'] = pd.to_numeric(BBDDca['ca_costo'])
-# BBDDca['ca_devacum21'] = pd.to_numeric(BBDDca['ca_devacum21'])
-# BBDDca['ca_dev22'] = pd.to_numeric(BBDDca['ca_dev22'])
-# BBDDca['ca_pia23'] = pd.to_numeric(BBDDca['ca_pia23'])
-# BBDDca['ca_pim23'] = pd.to_numeric(BBDDca['ca_pim23'])
-# BBDDca['ca_dev23'] = pd.to_numeric(BBDDca['ca_dev23'])
-# BBDDca['ca_devacumtot'] = pd.to_numeric(BBDDca['ca_devacumtot'])
-# BBDDca = BBDDca.fillna(0)
-# BBDDca['ca_devacum22'] = BBDDca['ca_devacum21'] + BBDDca['ca_dev22']
-
-# BBDDca = BBDDca[['cui','ca_costo','ca_devacum21','ca_dev22','ca_devacum22','ca_pia23','ca_pim23','ca_dev23','ca_devacumtot']]
-
-# BBDD['cui'] = pd.to_numeric(BBDD['cui'])
-# BBDDca['cui'] = pd.to_numeric(BBDDca['cui'])
-
-# BBDDca = BBDDca[BBDDca.ca_costo != ' ']
-# BBDDca = BBDDca[BBDDca.ca_costo != '']
-# BBDDca = BBDDca[BBDDca.cui != 2000028]
-
-# BBDD = pd.merge(BBDD, BBDDca, on='cui', how='left')
-
-# # variables calculadas
-
-# # maximo monto a financiar
-# BBDD['max_a_financ'] = BBDD['costoactpmi'] - BBDD['ca_devacum22'] - BBDD['ca_pim23']
-
-# BBDD.to_excel('{}{}'.format(PATH_OUTPUT,FILE_OUTPUT2),sheet_name='BD',index=False)
-
-
-
-
 
 driver.close()
 
